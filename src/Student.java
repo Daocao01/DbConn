6ac1a000b1f85b0718a74d7312fb8e95bda84103
Daocao01 <zhangli137850>
2021-04-02 17:17:10 +0800

    文件

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public static void main(String[] args){
+        int i;
+        for(i=0;i<=8;i++){
+            System.out.println(i);
+        }
+        System.out.println("1111");
+    }
+}
